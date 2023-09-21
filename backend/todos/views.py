6188c7d0cb5c6820ca5f1b1ee40d3d3f239b0f69
Mihail2403 <mihailpickalev@gmail.com>
2023-09-21 09:02:54 +0000

    Views for add and delete TODOs

--- backend/todos/views.py
+++ backend/todos/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from rest_framework import response
 from rest_framework.generics import ListAPIView
@@ -9,8 +10,27 @@ from .models import TODO
 # Create your views here.
 class TODOListAPIView(APIView):
     def get(self, request):
-        print(request.user.id)
-        todo = TODO.objects.filter(user_id=request.user.id)
-        serializer = TODOSerializer(todo, many=True)
-        return response.Response(serializer.data)
-
+        try:
+            print(request.user)
+            todo = TODO.objects.filter(user_id=request.user.id)
+            serializer = TODOSerializer(todo, many=True)
+            return response.Response(serializer.data)
+        except Exception as e:
+            print(e)
+            return response.Response({"status":"bad"})
+    def post(self, request):
+        try:
+            todo = TODO(text=request.body['text'], user=User.objects.get(id=request.user.id))
+            todo.save()
+            return response.Response({"status":"good", "todo_id":todo.id})
+        except Exception as e:
+            print(e)
+            return response.Response({"status":"bad"})
+    def delete(self, request):
+        try:
+            todo = TODO.objects.get(id=request.user.id)
+            todo.delete()
+            return response.Response({"status": "good", "del_todo_id": todo.id})
+        except Exception as e:
+            print(e)
+            return response.Response({"status": "bad"})
