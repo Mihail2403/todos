c1a30e5816f689deac0fbf826ee8f103dd10156e
Mihail2403 <mihailpickalev@gmail.com>
2023-09-23 09:05:32 +0000

    Set router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Registration/>
+    <router-view/>
   </div>
 </template>
 
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Registration from "@/views/Registration.vue";
+import {createRouter, createWebHashHistory} from "vue-router";
 
+const routes = [
+    {
+        path: '/reg',
+        component: Registration
+    },
+]
+const router = createRouter({
+    history:createWebHashHistory(),
+    routes
+})
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
